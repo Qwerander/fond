fa98ee55d234dfc3e40d66d665785d7c4108925e
Anton <antoha_denisenkov@yandex.ru>
2023-10-13 18:29:20 +0500

    поменял название

--- src/components/header/title/HeaderTitle.jsx
+++ src/components/header/title/HeaderTitle.jsx
@@ -3,7 +3,8 @@ import { HeaderTitleStl } from './headerTitle.styled';
 export const HeaderTitle = () => {
   return (
     <HeaderTitleStl>
-      <h1>Благотворительный фонд Василия</h1>
+      <h3>Благотворительный фонд</h3>
+      <h1>"Честь и достоинство"</h1>
       <span>
         в поддержку участников специальной военной операции и членов их семей
       </span>
